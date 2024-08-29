fb0dbd26283f8f360bf45a81f367ab8bc0cf49da
ne1s0n <nelson.nazzicari@crea.gov.it>
2024-08-29 11:11:48 +0200

    progress in data loading

--- d2r/dataset.py
+++ d2r/dataset.py
@@ -1,6 +1,10 @@
 #This module contains the information for one single dataset. When instantiated
 #it does not really read the data, but it checks for the required files to be existing
 
+from osgeo import gdal
+import numpy as np
+
+
 class Dataset:
 	def __init__(self, title, body):
 		self.name = title
@@ -30,9 +34,30 @@ class Dataset:
 			raise ValueError('Missing "type" field for dataset: ' + title)
 		
 	def to_string(self):
-		return(self.name + ' (' + self.type) + ')'
+		return(self.name + ' (' + self.type + ')') 
 	def get_meta(self):
 		return(self.meta)
 	def get_config(self):
 		return(self.config)
+		
+	def get_image(self):
+		ds = gdal.Open(self.orthomosaic_file, gdal.GA_ReadOnly)
 
+		print("Projection: ", ds.GetProjection())  # get projection
+		print("Columns:", ds.RasterXSize)  # number of columns
+		print("Rows:", ds.RasterYSize)  # number of rows
+		print("Band count:", ds.RasterCount)  # number of bands
+
+		output=np.zeros(shape=(ds.RasterYSize, ds.RasterXSize, 3))
+		output[:, :, 0] = ds.GetRasterBand(1).ReadAsArray()
+		output[:, :, 1] = ds.GetRasterBand(2).ReadAsArray()
+		output[:, :, 2] = ds.GetRasterBand(3).ReadAsArray()
+		return(output)
+
+	def get_channels(self):
+		return 'RGB'
+	def get_polygons(self):
+		### load and return ROIs as geometric polygons 
+		return None
+
+	
