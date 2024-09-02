e771cf20273e574c2c66f688d3d90fa7342ae944
ne1s0n <nelson.nazzicari@crea.gov.it>
2024-09-02 16:51:36 +0200

    new encoding of channels

--- d2r/indexes.py
+++ d2r/indexes.py
@@ -1,20 +1,16 @@
 #all the functions in this module share the same interface: first argument is
 #an image (numpy array, first two levels are x,y coordinates, third one is the
-#channels). Second argument is a dictionary where key are channel names, second
-#index is the corresponding channel, e.g.
-#	channels = {
-#		'R' : 0,
-#		'G' : 1,
-#		'B' : 2
-#	}
+#channels). Second argument is a list of channel names, which determine the
+#channel order. Some values are fixed: red, green, blue, rededge, nir (all
+#small caps, no spaces). 
 
 import numpy as np
 
 def GLI(img, channels):
 	"""Green leaf index"""
-	red = channels['red']
-	green = channels['green']
-	blue = channels['blue']
+	red = channels.index('red')
+	green = channels.index('green')
+	blue = channels.index('blue')
 	return(2 * img[:, :, green] - img[:, :, red] - img[:, :, blue]) / (2 * img[:, :, green] + img[:, :, red] + img[:, :, blue])   
 
 def random(img, channels):
