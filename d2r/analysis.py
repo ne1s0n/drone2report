916ab7e810129e391974254fa12a738aff0fad08
ne1s0n <nelson.nazzicari@crea.gov.it>
2024-09-10 12:50:38 +0200

    comments

--- d2r/analysis.py
+++ d2r/analysis.py
@@ -1,6 +1,7 @@
 import importlib
 
 def analysis_factory(title, config):
+	"""factory method for dynamically instantiating analysis classes"""
 	#importing the module
 	dynamic_module = importlib.import_module('d2r.analyses.' + title)
 	#importing the class
@@ -9,6 +10,13 @@ def analysis_factory(title, config):
 	return dynamic_class(title, config)
 
 class Analysis:
+	"""
+	This is the base Analysis class, to be subclassed by actual analyses
+	
+	Place your implementations (in the form of a subclass) under dr2/analyses.
+	Each module (file) will be dynamically loaded. Only the "run" method
+	will be called.
+	"""
 	def __init__(self, title, config):
 		self.title = title
 		self.config = config
@@ -17,10 +25,5 @@ class Analysis:
 		return(self.title)
 		
 	def run(self, dataset):
-		"""this method is ment to be overloaded by the derived subclasses, it's where the actual computation happens"""
+		"""this method is meant to be overloaded by the derived subclasses, it's where the actual computation happens"""
 		pass
-		
-class indexes(Analysis):
-	def run(self, dataset):
-		#TODO
-		return None
