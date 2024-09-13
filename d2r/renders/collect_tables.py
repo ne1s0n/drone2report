73e6db22bcabc49feabe0023976b988503bc0e5d
ne1s0n <nelson.nazzicari@crea.gov.it>
2024-09-13 12:01:50 +0200

    collectingn all csv tables into a single csv, based on table type

--- d2r/renders/collect_tables.py
+++ d2r/renders/collect_tables.py
@@ -3,35 +3,54 @@ import pathlib
 import warnings
 from d2r.render import Render
 import d2r.misc
+import pandas as pd
 
 import pprint
 
 class collect_tables(Render):
 	def run(self):
 		#room for results
-		path = pathlib.Path(self.config['outfolder'])
-		path.mkdir(parents=True, exist_ok=True)
+		outfolder = pathlib.Path(self.config['outfolder'])
+		outfolder.mkdir(parents=True, exist_ok=True)
 		
-		#first pass: building a dictionary type => file
-		infiles = {}
-		for infolder in self._parse_infolders():
-			#for all .csv files
+		#first pass: building a type => [files list] dictionary 
+		infiles = self._collect_files()
+		
+		#for each table type
+		for table_type in infiles:
+			#building a summary table
+			sumtab =  self._summary_table(infiles[table_type])
+			
+			#saving the table
+			outfile = os.path.join(outfolder, table_type + '.csv')
+			sumtab.to_csv(outfile, index=False)
+			print('Collected ' + table_type + ' table into ' + outfile)
+
+	def _summary_table(self, infiles):
+		res = None
+		for f in infiles:
+			res = pd.concat([res, pd.read_csv(f)])
+		return res
+
+	def _collect_files(self):
+		"""returns all interesting files in a type => [files list] dictionary"""
+		res = {}
+		for infolder in self._get_infolders():
+			#going through all files, keeping only correct ones
 			for f in os.listdir(infolder):
 				full_path = os.path.join(infolder, f)
 				table_type = self._get_table_type(full_path)
 				if table_type is None:
 					continue
 				#taking notes
-				if table_type in infiles: 
-					infiles[table_type].append(full_path)
+				if table_type in res: 
+					res[table_type].append(full_path)
 				else:
-					infiles[table_type] = [full_path]
-
-		#showing what we got
-		pprint.pprint(infiles)
-
-
-	def _parse_infolders(self):
+					res[table_type] = [full_path]
+		return(res)
+		
+	def _get_infolders(self):
+		"""collects all infolders from config"""
 		res = []
 		for key in self.config:
 			if key.lower().startswith('table_infolder'):
