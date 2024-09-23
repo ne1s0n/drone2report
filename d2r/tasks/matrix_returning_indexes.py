c72728e978301c6544be654e8d3d94862935e7f3
ne1s0n <nelson.nazzicari@crea.gov.it>
2024-09-23 15:00:22 +0200

    implemented indexes: BGI, PSRI, TVI_transformed, TVI_triangular, CVI, CIG, CIrededge, CIrededge710, EVI

--- d2r/tasks/matrix_returning_indexes.py
+++ d2r/tasks/matrix_returning_indexes.py
@@ -42,6 +42,80 @@ def GLI(img, channels):
 		(2.0*green + red + blue)
 	) 
 
+def CVI(img, channels):
+	"""Chlorophyll vegetation index, uses nir, red, green"""
+	try:
+		NIR   = img[:,:,channels.index('nir')]
+		red   = img[:,:,channels.index('red')]
+		green = img[:,:,channels.index('green')]
+	except ValueError:
+		#if this clause is activated it means that the requested channel(s) are not available
+		return np.nan
+	#if we get here the index can be applied to the current image
+	return(NIR * red / (green ** 2)) 
+
+def EVI(img, channels):
+	"""Enhanced Vegetation Index, uses nir, red, blue"""
+	try:
+		NIR  = img[:,:,channels.index('nir')]
+		red  = img[:,:,channels.index('red')]
+		blue = img[:,:,channels.index('blue')]
+	except ValueError:
+		#if this clause is activated it means that the requested channel(s) are not available
+		return np.nan
+	#if we get here the index can be applied to the current image
+	return(
+		2.5 * (NIR - red) /
+		(NIR + 6*red - 7.5*blue + 1)
+	) 
+
+def CIG(img, channels):
+	"""Chlorophyll Index Green, uses nir, green"""
+	try:
+		NIR   = img[:,:,channels.index('nir')]
+		green = img[:,:,channels.index('green')]
+	except ValueError:
+		#if this clause is activated it means that the requested channel(s) are not available
+		return np.nan
+	#if we get here the index can be applied to the current image
+	return(-1 + NIR/green) 
+
+def CIrededge(img, channels):
+	"""Chlorophyll IndexRedEdge, uses nir, rededge"""
+	try:
+		NIR     = img[:,:,channels.index('nir')]
+		rededge = img[:,:,channels.index('rededge')]
+	except ValueError:
+		#if this clause is activated it means that the requested channel(s) are not available
+		return np.nan
+	#if we get here the index can be applied to the current image
+	return(-1 + NIR/rededge) 
+
+def CIrededge710(img, channels):
+	"""Chlorophyll IndexRedEdge-710, uses 710, 750"""
+	try:
+		w710 = img[:,:,channels.index('710')]
+		w750 = img[:,:,channels.index('750')]
+	except ValueError:
+		#if this clause is activated it means that the requested channel(s) are not available
+		return np.nan
+	#if we get here the index can be applied to the current image
+	return(-1 + w750/w710) 
+
+
+
+def BGI(img, channels):
+	"""Simple Ratio 450/550 Blue green pigment index, uses 450, 550"""
+	try:
+		w450   = img[:,:,channels.index('450')]
+		w550 = img[:,:,channels.index('550')]
+	except ValueError:
+		#if this clause is activated it means that the requested channel(s) are not available
+		return np.nan
+	#if we get here the index can be applied to the current image
+	return(w450 / w550) 
+
+
 def HUE(img, channels):
 	"""Hue, uses red, green, blue"""
 	try:
@@ -55,6 +129,36 @@ def HUE(img, channels):
 	return(np.arctan(
 		(2*red - green - blue) * (green - blue) / 30.5
 	)) 
+
+def PSRI(img, channels):
+	"""Plant Senescence Reflectance Index, uses 500, 678, 750"""
+	try:
+		w500  = img[:,:,channels.index('500')]
+		w678  = img[:,:,channels.index('678')]
+		w750  = img[:,:,channels.index('750')]
+	except ValueError:
+		#if this clause is activated it means that the requested channel(s) are not available
+		return np.nan
+	#if we get here the index can be applied to the current image
+	return((w678 - w500) / w750) 
+
+def TVI_triangular(img, channels):
+	"""Triangular Vegetation Index, uses 550, 670, 750"""
+	try:
+		w550  = img[:,:,channels.index('550')]
+		w670  = img[:,:,channels.index('670')]
+		w750  = img[:,:,channels.index('750')]
+	except ValueError:
+		#if this clause is activated it means that the requested channel(s) are not available
+		return np.nan
+	#if we get here the index can be applied to the current image
+	return(0.5 * (120*(w750 - w550) - 200*(w670 - w550))) 
+
+
+
+def TVI_transformed(img, channels):
+	"""Transformed Vegetation Index, uses NDVI index """
+	return(np.sqrt(NDVI(img, channels) + 0.5))
 
 def GNDVI(img, channels):
 	"""Green Normalized Difference Vegetation Index, uses NIR, 540:570"""
