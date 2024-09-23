43399c655d2a18e9fa28ce7ff41be5cc15dcd61e
ne1s0n <nelson.nazzicari@crea.gov.it>
2024-09-23 11:44:50 +0200

    added indexes: GNDVI, RVI, NDRE, NGDRI

--- d2r/tasks/matrix_returning_indexes.py
+++ d2r/tasks/matrix_returning_indexes.py
@@ -34,6 +34,50 @@ def GLI(img, channels):
 	#if we get here the index can be applied to the current image
 	return  ((2.0 * img[:, :, green] - img[:, :, red] - img[:, :, blue]) / (2.0 * img[:, :, green] + img[:, :, red] + img[:, :, blue])) 
 
+def GNDVI(img, channels):
+	"""Green Normalized Difference Vegetation Index, uses NIR, 540:570"""
+	try:
+		greenYellow = channels.index('540:570')
+		NIR = channels.index('nir')
+	except ValueError:
+		#if this clause is activated it means that the requested channel(s) are not available
+		return np.nan
+	#if we get here the index can be applied to the current image
+	return((img[:, :, NIR] - img[:, :, greenYellow]) / (img[:, :, NIR] + img[:, :, greenYellow])) 
+
+def GNDVI(img, channels):
+	"""Simple Ratio 800/670 Ratio Vegetation Index, uses 800, 670"""
+	try:
+		w800 = channels.index('800')
+		w670 = channels.index('670')
+	except ValueError:
+		#if this clause is activated it means that the requested channel(s) are not available
+		return np.nan
+	#if we get here the index can be applied to the current image
+	return(img[:, :, w800] / img[:, :, w670])
+
+def NDRE(img, channels):
+	"""Normalized Difference NIR/Rededge Normalized Difference Red-Edge, uses NIR, rededge"""
+	try:
+		redEdge = channels.index('rededge')
+		NIR = channels.index('nir')
+	except ValueError:
+		#if this clause is activated it means that the requested channel(s) are not available
+		return np.nan
+	#if we get here the index can be applied to the current image
+	return((img[:, :, NIR] - img[:, :, redEdge]) / (img[:, :, NIR] + img[:, :, redEdge])) 
+
+def NGRDI(img, channels):
+	"""Normalized Difference Green/Red Normalized green red difference index, Visible Atmospherically Resistant Indices Green (VIgreen), uses green, red"""
+	try:
+		green = channels.index('green')
+		red = channels.index('red')
+	except ValueError:
+		#if this clause is activated it means that the requested channel(s) are not available
+		return np.nan
+	#if we get here the index can be applied to the current image
+	return((img[:, :, green] - img[:, :, red]) / (img[:, :, green] + img[:, :, red])) 
+
 def random_matrix(img, channels):
 	"""a random value between zero and one"""
 	return(np.random.rand(img.shape[0], img.shape[1]))   
