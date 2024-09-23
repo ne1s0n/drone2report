f5cbad155688b27a3687087021995d6d853cc1e6
ne1s0n <nelson.nazzicari@crea.gov.it>
2024-09-23 12:00:18 +0200

    added indexes: VARIgreen, VARIrededge, VARI700

--- d2r/tasks/matrix_returning_indexes.py
+++ d2r/tasks/matrix_returning_indexes.py
@@ -20,7 +20,10 @@ def NDVI(img, channels):
 		#if this clause is activated it means that the requested channel(s) are not available
 		return np.nan
 	#if we get here the index can be applied to the current image
-	return((img[:, :, NIR] - img[:, :, red]) / (img[:, :, NIR] + img[:, :, red])) 
+	return(
+		(img[:,:,NIR] - img[:,:,red]) / 
+		(img[:,:,NIR] + img[:,:,red])
+	) 
 
 def GLI(img, channels):
 	"""Green leaf index, uses red, green, blue"""
@@ -32,7 +35,10 @@ def GLI(img, channels):
 		#if this clause is activated it means that the requested channel(s) are not available
 		return np.nan
 	#if we get here the index can be applied to the current image
-	return  ((2.0 * img[:, :, green] - img[:, :, red] - img[:, :, blue]) / (2.0 * img[:, :, green] + img[:, :, red] + img[:, :, blue])) 
+	return(
+		(2.0*img[:,:,green] - img[:,:,red] - img[:,:,blue]) / 
+		(2.0*img[:,:,green] + img[:,:,red] + img[:,:,blue])
+	) 
 
 def GNDVI(img, channels):
 	"""Green Normalized Difference Vegetation Index, uses NIR, 540:570"""
@@ -43,7 +49,10 @@ def GNDVI(img, channels):
 		#if this clause is activated it means that the requested channel(s) are not available
 		return np.nan
 	#if we get here the index can be applied to the current image
-	return((img[:, :, NIR] - img[:, :, greenYellow]) / (img[:, :, NIR] + img[:, :, greenYellow])) 
+	return(
+		(img[:,:,NIR] - img[:,:,greenYellow]) / 
+		(img[:,:,NIR] + img[:,:,greenYellow])
+	) 
 
 def GNDVI(img, channels):
 	"""Simple Ratio 800/670 Ratio Vegetation Index, uses 800, 670"""
@@ -54,7 +63,7 @@ def GNDVI(img, channels):
 		#if this clause is activated it means that the requested channel(s) are not available
 		return np.nan
 	#if we get here the index can be applied to the current image
-	return(img[:, :, w800] / img[:, :, w670])
+	return(img[:,:,w800] / img[:,:,w670])
 
 def NDRE(img, channels):
 	"""Normalized Difference NIR/Rededge Normalized Difference Red-Edge, uses NIR, rededge"""
@@ -65,7 +74,10 @@ def NDRE(img, channels):
 		#if this clause is activated it means that the requested channel(s) are not available
 		return np.nan
 	#if we get here the index can be applied to the current image
-	return((img[:, :, NIR] - img[:, :, redEdge]) / (img[:, :, NIR] + img[:, :, redEdge])) 
+	return(
+		(img[:,:,NIR] - img[:,:,redEdge]) / 
+		(img[:,:,NIR] + img[:,:,redEdge])
+	) 
 
 def NGRDI(img, channels):
 	"""Normalized Difference Green/Red Normalized green red difference index, Visible Atmospherically Resistant Indices Green (VIgreen), uses green, red"""
@@ -76,7 +88,54 @@ def NGRDI(img, channels):
 		#if this clause is activated it means that the requested channel(s) are not available
 		return np.nan
 	#if we get here the index can be applied to the current image
-	return((img[:, :, green] - img[:, :, red]) / (img[:, :, green] + img[:, :, red])) 
+	return(
+		(img[:,:,green] - img[:,:,red]) / 
+		(img[:,:,green] + img[:,:,red])
+	) 
+
+def VARIgreen(img, channels):
+	"""Visible Atmospherically Resistant Index Green, uses 459:490, 545:565, 620:680"""
+	try:
+		w459 = channels.index('459:490')
+		w545 = channels.index('545:565')
+		w620 = channels.index('620:680')
+	except ValueError:
+		#if this clause is activated it means that the requested channel(s) are not available
+		return np.nan
+	#if we get here the index can be applied to the current image
+	return(
+		(img[:,:,w545] - img[:,:,w620]) / 
+		(img[:,:,w545] + img[:,:,w620] - img[:,:,w459])
+	) 
+
+def VARI700(img, channels):
+	"""Visible Atmospherically Resistant Indices 700, uses 470:490, 660:680, 700"""
+	try:
+		w470 = channels.index('470:490')
+		w660 = channels.index('660:680')
+		w700 = channels.index('700')
+	except ValueError:
+		#if this clause is activated it means that the requested channel(s) are not available
+		return np.nan
+	#if we get here the index can be applied to the current image
+	return(
+		(img[:,:,w700] - 1.7*img[:,:,w660] + 0.7*img[:,:,w470]) /
+		(img[:,:,w700] + 2.3*img[:,:,w660] - 1.3*img[:,:,w470]) 
+	) 
+
+def VARIrededge(img, channels):
+	"""Visible Atmospherically Resistant Indices RedEdge, uses 700:710, 620:680"""
+	try:
+		w700 = channels.index('700:710')
+		w620 = channels.index('620:680')
+	except ValueError:
+		#if this clause is activated it means that the requested channel(s) are not available
+		return np.nan
+	#if we get here the index can be applied to the current image
+	return(
+		(img[:,:,w700] - img[:,:,w620]) /
+		(img[:,:,w700] + img[:,:,w620])
+	) 
 
 def random_matrix(img, channels):
 	"""a random value between zero and one"""
